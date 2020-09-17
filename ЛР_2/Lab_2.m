09841b455ff521510161933c384057bb0135c1c5
d3vyatk4ru <danya.devyatkin@mail.ru>
2020-09-17 12:12:04 +0300

    Отчет.
    Добавлен отчет. задание а и б. Код сделан красивым.

--- ЛР_2/Lab_2.m
+++ ЛР_2/Lab_2.m
@@ -13,13 +13,25 @@ end
 disp('Якобиан системы: ')
 J = jacobian([3*x+3*y+x*y+y^2, x+y^2],[x, y]);
 
-
 for i = 1:3
+   
     fprintf('Для %d-го положения равновесия:\n', i);
-    disp('1) матрица системы первого приближения:')
+    disp('1) матрица системы первого приближения:');
+    
+    % calculating jacobian in points
     subs_J = subs(J, [x y] ,[s.x(i) s.y(i)]);
     disp(subs_J)
     disp('2) cобственные значения: ')
+    
+    % finding eigen values
     r = eig(subs_J);
     fprintf('%d %d\n\n', [r(1,1), r(2,1)]);
 end
+
+
+n = 100;
+m = 100;
+p = 100;
+
+[X, Y, Z] = meshgrid(1:n, 1:m, 1:p);
+streamslice(X, Y, Z, 3*X + 3*Y + X.*Y +Y.*Y);
